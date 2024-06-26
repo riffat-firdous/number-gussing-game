e96640df23c184159ac3c8f9f7e375aca82cd81e
riffat-firdous <160425768+riffat-firdous@users.noreply.github.com>
2024-06-26 08:38:27 -0700

    numner-gussing-game

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+
+console.log ("\n \t Wellcome to code with Riffat Firdous - CLI Number Guessing Game\n");
+
+const randomNumber = Math.floor(Math.random() * 5 +1);
+
+const answer = await inquirer.prompt([
+
+{
+    name:"userGuessNumber",
+    type:"number",
+    message:"Enter your guess number(Numner Limit from 1 to 5):",
+},
+]
+);
+
+if (answer.userGuessNumber === randomNumber){
+
+ console.log("Congratulation ! you guess a coreect number.");
+
+}
+else
+{
+
+    console.log("Sorry you are guess a wrong number");
+
+}
